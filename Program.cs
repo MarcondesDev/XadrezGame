81c1fffdca2b1898c7fcbd5a2a195dc8b57a17d4
Marcondes Ferreira <formyjobMascou@gmail.com>
2021-10-15 13:45:01 -0300

    Comecando a implementar o Tabuleiro

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace Xadrex
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3,4);
-            System.Console.WriteLine("Posicao: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            
+            
             Console.ReadLine();
         }
     }
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+namespace tabuleiro
+{
+    public class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qteMovimentos { get; set; }
+        public Tabuleiro tab {get;set;}
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor) {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+        }
+    }
+}
