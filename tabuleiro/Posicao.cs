6b15c70bca168ed9c72542adc5b98e0372c13b77
Marcondes Ferreira <formyjobMascou@gmail.com>
2021-10-15 13:10:08 -0300

    Criando a class posicao

--- tabuleiro/Posicao.cs
+++ tabuleiro/Posicao.cs
@@ -5,6 +5,10 @@ namespace tabuleiro
     {
         public int Linha { get; set; }
         public int Coluna { get; set; }
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
         
         public override string ToString() {
             return Linha + ", " + Coluna;
