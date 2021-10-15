d7226d673b920054ae06a897da0a08844bf3e868
Marcondes Ferreira <formyjobMascou@gmail.com>
2021-10-15 18:42:36 -0300

    Comecando a implementar um metodo que imprimir o tabuleiro

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Xadrex
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
             
+            Tela.imprimirTabuleiro(tab);
             
             Console.ReadLine();
         }
---
+++ Tela.cs
@@ -0,0 +1,21 @@
+using System;
+using tabuleiro;
+
+namespace Xadrex
+{
+    public class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab) {
+            for (int i = 0; i<tab.linhas; i++) {
+                for (int j = 0; j<tab.colunas; j++) {
+                    if(tab.peca(i,j) == null) {
+                        System.Console.Write("- ");
+                    } else {
+                        System.Console.Write(tab.peca(i,j) + " ");
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }        
+    }
+}
