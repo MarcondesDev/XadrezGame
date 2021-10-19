b8e869074d19f3ba90df49b1d976055d90bcec8f
Marcondes Ferreira <formyjobMascou@gmail.com>
2021-10-19 09:41:24 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace Xadrex
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
             tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
             
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
+            
             Tela.imprimirTabuleiro(tab);
             
             Console.ReadLine();
